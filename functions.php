d49b89017bb4df13fd252937cd7465d572dbc15a
sandip <sandip@codepixelzmedia.com.np>
2018-07-20 08:59:53 +0545

    sailung theme initial steps

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php 
+function  sailung_theme_setup(){
+	wp_enqueue_style('style' , get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts','sailung_theme_setup');
+// Navigation menu
+register_nav_menus(array('primary'=> __('Primary Menu'),'footer'=>__('Footer Menu'),));
+//Get top ancestor
+function get_top_ancestor_id(){
+	global $post;
+	if($post->post_parent){
+		$ancestors=array_reverse(get_post_ancestors($post->ID));
+		return $ancestors[0];
+	}
+	return $post->ID;
+}
---
+++ page.php
@@ -0,0 +1,18 @@
+		<?php 
+		get_header();
+		if (have_posts()): while (have_posts()): the_post();?>
+			<article class="post page">
+				<span class="parent-link"><a href="<?php echo get_the_permalink(get_top_ancestor_id());?>"><?php echo get_the_title(get_top_ancestor_id()); ?></a></span>
+				<?php 
+				$args=array('child_of'=>get_top_ancestor_id(),'title_li'=>'');
+				?>
+				<?php wp_list_pages($args); ?>
+			<h1><?php the_title(); ?></h1>
+			<?php the_content();?>
+		</article>
+		<?php endwhile; 
+		else :
+			echo "<p> No content Found </p>";
+		endif;
+		get_footer();
+		?>
